cb8138d69815f92b1a219ce524327f0e59918548
Vadim Rusu <ruvamd@gmail.com>
2022-07-28 10:03:33 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -46,13 +46,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites",
         'django.contrib.staticfiles',
         'blango_auth',
         'blog',
         "crispy_forms",
         "crispy_bootstrap5",
-        "debug_toolbar",
-        
+        "debug_toolbar", 
+        "allauth", 
+        "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google"
     ]
 
     MIDDLEWARE = [
@@ -204,7 +208,11 @@ class Dev(Configuration):
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
     REGISTRATION_OPEN = True
-
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
--- blango/urls.py
+++ blango/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register",),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
 
 if settings.DEBUG:
