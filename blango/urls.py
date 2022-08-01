fa4b90f9f4c074bd193eb1ccf6b40716574bf491
Vadim Rusu <ruvamd@gmail.com>
2022-08-01 02:29:24 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register",),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("allauth.urls")),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
