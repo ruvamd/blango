455e72b8dad0291ece21fe5dc2ca3f480856ad3e
Vadim Rusu <ruvamd@gmail.com>
2022-08-18 05:16:28 +0000

    Finish authentication

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,11 +2,12 @@ from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
-
+from rest_framework.authentication import SessionAuthentication
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    authentication_classes = [SessionAuthentication]
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
