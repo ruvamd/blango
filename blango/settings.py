ef5a797aa76aa74c320ebed55ef6cf36fd6debba
Vadim Rusu <ruvamd@gmail.com>
2022-09-05 08:34:15 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from configurations import Configuration,values
 import dj_database_url
+from datetime import timedelta
 
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,6 +76,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
         ],
 
         "DEFAULT_PERMISSION_CLASSES": [
@@ -258,6 +260,11 @@ class Dev(Configuration):
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
 class Prod(Dev):
     DEBUG = False
